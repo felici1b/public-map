86958ec389218baaac9cafa0faac1d9affe4d120
felici1b <45767969+felici1b@users.noreply.github.com>
2022-10-19 12:31:25 +0200

    test new card

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 US counties
 https://github.com/zeke/us-counties/blob/master/county.geo.json
+https://public.opendatasoft.com/explore/dataset/us-county-boundaries/table
--- reduce.py
+++ reduce.py
@@ -1,7 +1,9 @@
 import json
 from copy import deepcopy
 
-FILE = 'county-50mo.geo.json'
+INPUT_FILE_NAME = 'county.geo.json'
+OUTPUT_FILE_NAME = 'county.reduced.geo.json'
+
 FIELED_TO_KEEP = [
     "STATEFP10",
     "COUNTYFP10",
@@ -16,23 +18,33 @@ FIELED_TO_KEEP = [
 
 LAT = [23, 50]
 LONG = [-132, -64]
+#LAT = []
+#LONG = []
 
-json_file = json.load(open(FILE))
+STATES = []
+
+json_file = json.load(open(INPUT_FILE_NAME))
 
 reduced_json_file = {
-    "type":"FeatureCollection",
+    "type": "FeatureCollection",
     "features": []
 }
 
-for idx, e in enumerate(json_file["features"]):
+
+def is_filtered(e):
+    cond_coord = len(LAT) == 0 or (LAT[0] < float(e["properties"]["INTPTLAT10"]) < LAT[1] and LONG[0] < float(e["properties"]["INTPTLON10"]) < LONG[1])
+    cond_state = len(STATES) == 0 or e["properties"]["state"] in STATES
+    return cond_state and cond_coord
+
+
+for e in json_file["features"]:
     e_reduced = deepcopy(e)
     for k in e["properties"].keys():
         if k not in FIELED_TO_KEEP:
             del e_reduced["properties"][k]
 
-    if LAT[0] < float(e["properties"]["INTPTLAT10"]) < LAT[1] and LONG[0] < float(e["properties"]["INTPTLON10"]) < LONG[1]:
+    if is_filtered(e):
         reduced_json_file["features"].append(e_reduced)
 
-
-with open(f"reduced.{FILE}", "w+") as f:
+with open(OUTPUT_FILE_NAME, "w+") as f:
     json.dump(reduced_json_file, f, separators=(',', ':'))
